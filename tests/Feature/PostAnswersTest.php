df43da1e4ec72fe7ded303716b52ff293d3ccae3
jiyr <jiyr@boyaa.com>
2021-12-09 18:03:54 +0800

    post answer

--- tests/Feature/PostAnswersTest.php
+++ tests/Feature/PostAnswersTest.php
@@ -10,11 +10,13 @@ use Tests\TestCase;
 
 class PostAnswersTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
-    public function user_can_post_an_answer_to_a_question()
+    public function user_can_post_an_answer_to_a_published_question()
     {
 //        1. 假设存在问题
-        $questions = Question::factory()->create();
+        $questions = Question::factory()->published()->create();
         $user = User::factory()->create();
 //        2. 访问路由
         $response = $this->post("/questions/{$questions->id}/answers", [
@@ -26,5 +28,23 @@ class PostAnswersTest extends TestCase
         $answer = $questions->answers()->where('user_id', $user->id)->first();
         $this->assertNotNull($answer);
         $this->assertEquals(1, $questions->answers()->count());
+    }
+
+    /** @test */
+    public function can_not_post_an_answer_to_an_unpublished_question()
+    {
+        $question = Question::factory()->unpublished()->create();
+        $user = User::factory()->create();
+
+        $response = $this->withExceptionHandling()
+            ->post("/questions/{$question->id}/answers", [
+                'user_id' => $user->id,
+                'content' => 'This is an answer.'
+            ]);
+
+        $response->assertStatus(404);
+
+        $this->assertDatabaseMissing('answers', ['question_id' => $question->id]);
+        $this->assertEquals(0, $question->answers()->count());
     }
 }
